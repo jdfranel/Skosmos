2ba5edc18d67d8062c321d8f7173450e4324552e
UnniKohonen <45235116+UnniKohonen@users.noreply.github.com>
2026-03-19 13:19:30 +0200

    Add a button to collapse sidebar on mobile

---
+++ resource/js/mobile.js
@@ -0,0 +1,18 @@
+// Register event listener for clicking on sidebar collapse button that toggles visibility of sidebar
+const sidebarCollapseBtn = document.getElementById('sidebar-collapse-btn')
+
+sidebarCollapseBtn.addEventListener('click', () => {
+  // Toggle sidebar visibility
+  const sidebar = document.getElementById('sidebar-col')
+  sidebar.classList.toggle('d-none')
+
+  // Toggle fontawesome icon
+  const i = document.querySelector('#sidebar-collapse-btn i')
+  i.classList.toggle('fa-chevron-right')
+  i.classList.toggle('fa-chevron-down')
+
+  // Toggle button text
+  sidebarCollapseBtn.getElementsByTagName('span')[0].textContent = sidebar.classList.contains('d-none') ? 'Selaa käsitteitä' : 'Piilota käsitteet'
+})
+
+
